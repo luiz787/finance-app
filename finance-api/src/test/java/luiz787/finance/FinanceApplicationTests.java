590c51adf7bc984c1b641912c269965f0f07d227
Luiz Berto <diasbertoluiz@gmail.com>
2020-02-08 11:37:49 -0300

    Commit inicial.

---
+++ finance-api/src/test/java/luiz787/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package luiz787.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
